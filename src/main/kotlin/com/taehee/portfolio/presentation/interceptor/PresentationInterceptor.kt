5c18bf8970ad09df444359625f3f5a59888673d9
taeheee02 <fullsun2002@naver.com>
2025-02-17 13:25:59 +0900

    인터셉터 개발

--- src/main/kotlin/com/taehee/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/taehee/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,26 @@
 package com.taehee.portfolio.presentation.interceptor
 
+import com.taehee.portfolio.domain.entity.HttpInterface
+import com.taehee.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
