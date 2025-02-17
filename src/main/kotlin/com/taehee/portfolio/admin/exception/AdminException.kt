4ea8316e0e770fa1ed0a1c84948a7c1002373499
taeheee02 <fullsun2002@naver.com>
2025-02-18 01:38:51 +0900

    Admin 공통 개발 - Admin & Exception

--- src/main/kotlin/com/taehee/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/taehee/portfolio/admin/exception/AdminException.kt
@@ -9,3 +9,12 @@ abstract class AdminException(
     val httpStatus: HttpStatus = httpStatus
 
 }
+
+class AdminBadRequestException(message: String) : AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message
+)
+class AdminInternalServerErrorException(message: String) : AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message
+)
