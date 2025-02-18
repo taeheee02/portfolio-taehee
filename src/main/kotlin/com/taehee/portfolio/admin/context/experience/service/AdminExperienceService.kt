9fde4306e5f648222089a6cac44f3d52eabca7ee
taeheee02 <fullsun2002@naver.com>
2025-02-18 19:03:24 +0900

    Admin 삽입,수정 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/taehee/portfolio/admin/context/experience/service/AdminExperienceService.kt
+++ src/main/kotlin/com/taehee/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -1,11 +1,14 @@
 package com.taehee.portfolio.admin.context.experience.service
 
+import com.taehee.portfolio.admin.context.experience.form.ExperienceForm
 import com.taehee.portfolio.admin.data.TableDTO
 import com.taehee.portfolio.admin.exception.AdminBadRequestException
 import com.taehee.portfolio.domain.entity.Experience
 import com.taehee.portfolio.domain.entity.ExperienceDetail
 import com.taehee.portfolio.domain.repository.ExperienceRepository
+import com.taehee.portfolio.presentation.dto.ExperienceDTO
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminExperienceService(
@@ -27,4 +30,46 @@ class AdminExperienceService(
 
         return TableDTO.from(classInfo, entities)
     }
+
+    @Transactional
+    fun save(form : ExperienceForm) {
+        val experienceDetails = form.details
+            ?.map{ detail -> detail.toEntity() }
+            ?.toMutableList()
+        val experience = form.toEntity()
+        experience.addDetails(experienceDetails)
+
+        experienceRepository.save(experience)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ExperienceForm) {
+
+        val experience = experienceRepository.findById(id)
+            .orElseThrow{ throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수가 없습니다.")}
+        experience.update(
+            title = form.title,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = experience.details.map {it.id to it}.toMap()
+        form.details?.forEach {
+             val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    isActive = it.isActive
+                )
+            } else {
+                experience.details.add(it.toEntity())
+            }
+        }
+    }
+
+
 }
---
+++ src/main/kotlin/com/taehee/portfolio/admin/context/project/controller/AdminProjectApiController.kt
@@ -0,0 +1,35 @@
+package com.taehee.portfolio.admin.context.project.controller
+
+import com.taehee.portfolio.admin.context.project.form.ProjectForm
+import com.taehee.portfolio.admin.context.project.service.AdminProjectService
+import com.taehee.portfolio.admin.data.ApiResponse
+import com.taehee.portfolio.admin.data.TableDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestControllerAdvice
+@RequestMapping("/admin/api/projects")
+class AdminProjectApiController(
+    private val adminProjectService: AdminProjectService
+) {
+
+    @PostMapping
+    fun post (@RequestBody @Validated form: ProjectForm): ResponseEntity<Any> {
+        adminProjectService.save(form)
+
+        return ApiResponse.successCreate()
+    }
+
+    @PutMapping("/{id}")
+    fun putProject(@PathVariable id: Long, @RequestBody form: ProjectForm): ResponseEntity<Any> {
+        adminProjectService.update(id, form)
+
+        return ApiResponse.successUpdate()
+    }
+
+    @GetMapping("/{id}/details")
+    fun getProjectDetails(@PathVariable id: Long): TableDTO {
+        return adminProjectService.getProjectDetailTable(id)
+    }
+}
